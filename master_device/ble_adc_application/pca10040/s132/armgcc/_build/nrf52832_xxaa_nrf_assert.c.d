9b068755465a488584ac1ac5cf2dfdea578a8f09
Josh Chen <hsuan-mingchen2021@u.northwestern.edu>
2019-07-16 13:19:43 -0500

    Added nRF52 hardware code

---
+++ master_device/ble_adc_application/pca10040/s132/armgcc/_build/nrf52832_xxaa_nrf_assert.c.d
@@ -0,0 +1,131 @@
+_build/nrf52832_xxaa_nrf_assert.c.o: \
+ ../../../../components/libraries/util/nrf_assert.c \
+ ../../../../components/libraries/util/nrf_assert.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdint.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdint.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_default_types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\features.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_newlib_version.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_intsup.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_stdint.h \
+ ../../../../components/device/nrf.h \
+ ../../../../components/device/nrf52.h \
+ ../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../components/toolchain/system_nrf52.h \
+ ../../../../components/device/nrf52_bitfields.h \
+ ../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../components/device/nrf52_name_change.h \
+ ../../../../components/device/compiler_abstraction.h \
+ ../../../../components/libraries/util/app_error.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdio.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_ansi.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\newlib.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\config.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\ieeefp.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\cdefs.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stddef.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdarg.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\reent.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_ansi.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\lock.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\stdio.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdbool.h \
+ ../../../../components/libraries/util/sdk_errors.h \
+ ../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../components/libraries/util/nordic_common.h \
+ ../../../../components/libraries/util/app_error_weak.h
+
+../../../../components/libraries/util/nrf_assert.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdint.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdint.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_default_types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\features.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_newlib_version.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_intsup.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_stdint.h:
+
+../../../../components/device/nrf.h:
+
+../../../../components/device/nrf52.h:
+
+../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../components/toolchain/system_nrf52.h:
+
+../../../../components/device/nrf52_bitfields.h:
+
+../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../components/device/nrf52_name_change.h:
+
+../../../../components/device/compiler_abstraction.h:
+
+../../../../components/libraries/util/app_error.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdio.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_ansi.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\newlib.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\config.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\ieeefp.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\cdefs.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stddef.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdarg.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\reent.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_ansi.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\lock.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\stdio.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdbool.h:
+
+../../../../components/libraries/util/sdk_errors.h:
+
+../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../components/libraries/util/nordic_common.h:
+
+../../../../components/libraries/util/app_error_weak.h:
---
+++ master_device/ble_adc_application/pca10040/s132/armgcc/_build/nrf52832_xxaa_sdk_errors.c.d
@@ -0,0 +1,35 @@
+_build/nrf52832_xxaa_sdk_errors.c.o: \
+ ../../../../components/libraries/util/sdk_errors.c \
+ ../../../../components/libraries/util/sdk_errors.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdint.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdint.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_default_types.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\features.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_newlib_version.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_intsup.h \
+ d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_stdint.h \
+ ../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../components/libraries/util/nordic_common.h
+
+../../../../components/libraries/util/sdk_errors.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\lib\gcc\arm-none-eabi\5.4.1\include\stdint.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\stdint.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\machine\_default_types.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\features.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\_newlib_version.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_intsup.h:
+
+d:\applications\gnu\ tools\ arm\ embedded\5.4\ 2016q2\arm-none-eabi\include\sys\_stdint.h:
+
+../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../components/libraries/util/nordic_common.h:
